48c068836fb3a244d7339e64bd17920ebe99ff22
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-09-04 16:58:03 +0000

    gestion des entrée du joueur 1

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -12,3 +12,28 @@ def initialiser_grille():
 grille = initialiser_grille()
 
 afficher_grille(grille)
+
+#Postion de jouer 
+def demander_position_joueur(joueur):
+    while True:
+        try:
+            position = int(input(f"Joueur {joueur}, entrez un numéro de case (1-9):")) -1
+            if position < 0 or position > 8:
+                raise ValueError
+            return divmod(position, 3)
+
+        except ValueError:
+            print("Entrée invalide. Veuillez entrer un numéro de case entre 1 et 9")
+#Fonction pour jouer la partie
+def jouer_tour(grille, joueur):
+    position = demander_position_joueur(joueur)
+    if grille[position[0]][position[1]] == " ":
+        grille[position[0]][position[1]] = joueur
+    else:
+        print("Case déja occupé. Choisisszer une autre case.")
+        jouer_tour(grille, joueur)
+
+#Exécution du tour de jeu pour un joueur
+joueur_actuel = "X"
+jouer_tour(grille, joueur_actuel)
+afficher_grille(grille)
